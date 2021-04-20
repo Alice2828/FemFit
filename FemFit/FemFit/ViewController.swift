06774d713d48e1f4ef9d8925494f3f5168d1afc0
curer13 <era.owl@gmail.com>
2021-04-20 11:28:11 +0600

    first commit

---
+++ FemFit/FemFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FemFit
+//
+//  Created by User on 20.04.2021.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
