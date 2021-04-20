e812c0412aa32754b629aae1da04c5c750b85fa2
curer13 <era.owl@gmail.com>
2021-04-20 13:04:32 +0600

    login request

--- FemFit/FemFit/ViewController.swift
+++ FemFit/FemFit/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
-
+//let session = URLSession.shared
+//let url = URL(string: "https://wger.de/api/v2/login")!
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func logIn(_ sender: UIButton) {
+        let login = loginTF.text
+        let password = passwordTF.text
+        
+        guard let url = URL(string:  "https://wger.de/api/v2/login/") else {return}
+        
+        var json = [String:Any]()
+        json["username"] = login
+        json["password"] = password
+        do {
+            let data = try JSONSerialization.data(withJSONObject: json, options: [])
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = data
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
 
-
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                print(response ?? "def")
+                print(String(data: data!, encoding: .utf8) ?? "def") //Try this too!
+                }.resume()
+        }catch{
+        }
+    }
+    
 }
 
