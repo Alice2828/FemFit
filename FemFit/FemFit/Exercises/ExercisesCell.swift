c43aef1196117948427c0ab4821210829388eed2
leila <leilatheleph@gmail.com>
2021-05-06 13:41:31 +0600

    reset merge

--- FemFit/FemFit/Exercises/ExercisesCell.swift
+++ FemFit/FemFit/Exercises/ExercisesCell.swift
@@ -9,11 +9,55 @@
 import UIKit
 
 class ExercisesCell: UITableViewCell {
+    @IBOutlet weak var exerciseImage: UIImageView!
+    var exerciseIndex: Int?
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var likeBtn: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBAction func addToFavList(_ sender: UIButton) {
+        if favExercisesList.isEmpty{
+            for i in 0...exercisesList.count-1{
+                if exercisesList[i].id == exerciseIndex{
+                    favExercisesList.append(exercisesList[i])
+                    
+                    sender.tintColor = UIColor(named: "SelectionColor")
+                    
+                    break
+                }
+            }
+        }else{
+            for i in 0...favExercisesList.count-1{
+                if favExercisesList[i].id == exerciseIndex{
+                    favExercisesList.remove(at: i)
+                    sender.tintColor = UIColor(named: "WhiteUniversal")
+                    break
+                }
+                if i == favExercisesList.count-1{
+                    for i in 0...exercisesList.count-1{
+                        if exercisesList[i].id == exerciseIndex{
+                            favExercisesList.append(exercisesList[i])
+                            sender.tintColor = UIColor(named: "SelectionColor")
+                            break
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        nameLabel.layer.masksToBounds = true
+        nameLabel.layer.cornerRadius = 10
+        authorLabel.layer.masksToBounds = true
+        authorLabel.layer.cornerRadius = 10
+       // exerciseImage.layer.cornerRadius = 10
+        cellView.layer.cornerRadius = 10
+        
         // Initialization code
     }
 
