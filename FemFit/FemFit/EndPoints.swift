b6e360e676e57634c14a81591832e891ccc47fdd
leila <leilatheleph@gmail.com>
2021-05-09 12:07:06 +0600

    references removed

---
+++ FemFit/FemFit/EndPoints.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoints.swift
+//  FemFit
+//
+//  Created by User on 01.05.2021.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import Foundation
+let urlString = "https://wger.de/api/v2/"
+var exercisePath = "exercise/"
+var ingredientPath = "ingredient/"
